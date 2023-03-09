c93ef2717161e48a4c1e24d684341c9ff4fa0118
The Interview Sage <github@theinterviewsage.com>
2023-03-08 23:52:04 -0800

    Update

--- solutions/1275-Find Winner on a Tic Tac Toe Game.java
+++ solutions/1275-Find Winner on a Tic Tac Toe Game.java
@@ -17,30 +17,30 @@ class Solution {
             throw new IllegalArgumentException("Input move list is invalid");
         }
 
-        int n = 3;
-        int[] rows = new int[n];
-        int[] cols = new int[n];
+        int numMoves = moves.length;
+        int[] rows = new int[3];
+        int[] cols = new int[3];
         int diag1 = 0;
         int diag2 = 0;
-        int numMoves = moves.length;
 
         for (int i = 0; i < numMoves; i++) {
             int r = moves[i][0];
             int c = moves[i][1];
-            int val = i % 2 == 0 ? 1 : -1;
-            rows[r] += val;
-            cols[c] += val;
+            int player = (i & 1) == 0 ? 1 : -1;
+            rows[r] += player;
+            cols[c] += player;
             if (r == c) {
-                diag1 += val;
+                diag1 += player;
             }
-            if (r + c == n - 1) {
-                diag2 += val;
+            if (r + c == 2) {
+                diag2 += player;
             }
-            if (Math.abs(rows[r]) == n || Math.abs(cols[c]) == n || Math.abs(diag1) == n || Math.abs(diag2) == n) {
+
+            if (Math.abs(rows[r]) == 3 || Math.abs(cols[c]) == 3 || Math.abs(diag1) == 3 || Math.abs(diag2) == 3) {
                 if (i != numMoves - 1) {
                     throw new IllegalArgumentException("Input move list is invalid");
                 }
-                return val == 1 ? "A" : "B";
+                return player == 1 ? "A" : "B";
             }
         }
 
